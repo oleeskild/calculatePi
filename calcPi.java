65d3cb7d50d5f0f94ec0a8331415bf006c91f6c5
Ole Eskild <oleeskilds@gmail.com>
2014-10-07 16:13:37 +0200

    Made code cleaner

--- calcPi.java
+++ calcPi.java
@@ -1,17 +1,25 @@
 import java.util.Random;
 public class calcPi{
     public static void main(String[] args){
-        Random r = new Random();
-        double[] num = new double[16000];
-        for(int i = 0; i<num.length; i++){
-            num[i] = r.nextDouble();
+        
+        if(args.length == 0)
+            System.out.println("Usage: java calcPi [number of random pairs] ex. java calcPi 64000");
+        else{
+
+            Random r = new Random();
+            double[] xPos = new double[Integer.parseInt(args[0])];
+            double[] yPos = new double[Integer.parseInt(args[0])];
+            for(int i = 0; i<xPos.length; i++){
+                xPos[i] = r.nextDouble();
+                yPos[i] = r.nextDouble();
+            }
+            int counter = 0;
+            for(int i = 0; i<xPos.length; i++){
+                if((xPos[i]*xPos[i]) + (yPos[i]*yPos[i])<1)
+                    counter++;
+            }
+            System.out.println((counter*4.0)/(xPos.length));
         }
-        int counter = 0;
-        for(int i = 0; i<num.length/2; i++){
-            if((Math.pow(num[i],2) + Math.pow(num[(num.length/2)+i],2))<1)
-                counter++;
-        }
-        System.out.println((counter*4.0)/(num.length/2.0));
     }
 
 }
